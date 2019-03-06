b5ed078d58a62cc4cc202049a18dcfade5a7143c
Ben Kates <benekates@gmail.com>
2019-03-05 20:24:18 -0500

    refined text extraction again & added folder to output df

--- compassred-text-mining.R
+++ compassred-text-mining.R
@@ -48,56 +48,33 @@ for (blog_post in c(blog,blog_older_pages)){
       %>% html_attr('href'))
 }
 
-
 #scrape content
+scrape_df <- tibble(node = character(),folder = character())
+
 for (folder in c(services,solutions,about,employees,careers,blog_posts)){
   
   #scrape page and extract html nodes with class "sqs-block-content" (commonly used on page for main content)
-  if (!exists("html_body")){
-  html_body <- html_nodes(getCR(folder),css=".sqs-block-content")
-  }
-  else{
-    html_body <- html_body %>%
-      c(html_nodes(getCR(folder),css=".sqs-block-content"))
-  }
+  html_body <- html_text(html_nodes(getCR(folder),".sqs-block-content"))
+  scrape_df <- scrape_df %>% add_row(node=html_body,folder=folder)
   
   Sys.sleep(1) #don't overwhelm the site
 } 
 
-#create output vector then iterate through nodes (avoid the ones we don't want)
-text_vec <- character()
-for (html_snippet in html_body){
-  
-  #check to make sure there's content
-  if (length(html_nodes(html_snippet,"h1")) > 0 & length(html_nodes(html_snippet,xpath="p|blockquote")) == 0){next}
-  
-  #check for footer
-  footer_text <- "CompassRed, Inc. | All rights reserved. © 2018 | wilmington, de | philadelphia, pa | "
-  text <- html_text(html_snippet)
-  if (substr(text, 0, nchar(footer_text)) == footer_text){next}
-  
-  #check for newsletter
-  newsletter_text <- "Sign up for weekly data stories @ The Data Lab"
-  text <- html_text(html_snippet)
-  if (substr(text, 0, nchar(newsletter_text)) == newsletter_text){next}
-  
-  #extract text from p nodes
-  content_nodes <- html_nodes(html_snippet,xpath="p|blockquote")
-  for (node in content_nodes){
-    text_vec <- append(text_vec, html_text(node), after = length(text_vec))
-    
-  }
-}
-
+#remove text that looks like the footer
+footer_text <- "CompassRed, Inc. \\| All rights reserved"
+scrape_df <- scrape_df[!(grepl(footer_text,scrape_df$node)),]
 
 #TRANSFORMING####
 #tokenize words
-text_list <- as_tibble(text_vec)
-text_tokenized <- text_list %>% unnest_tokens(word,value)
+text_tokenized <- scrape_df %>% unnest_tokens(word,node)
 
 #take out stop words and count
 data(stop_words)
-text_tokenized %>% anti_join(stop_words) %>%
-  group_by(word) %>% summarise(count=n()) %>%
+text_df <- text_tokenized %>% anti_join(stop_words) %>%
+  group_by(folder,word) %>% summarise(count=n()) %>%
   arrange(desc(count))
+
+
+
+
 
